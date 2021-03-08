853a0130ccdb3d8ca6255c143febb9afb396b612
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-03-08 15:43:10 +0100

    Refactor bedrock info into bedrock game

--- src/main/java/de/timmi6790/mpstats/api/versions/v1/bedrock/info/BedrockInfoService.java
+++
@@ -1,7 +0,0 @@
-package de.timmi6790.mpstats.api.versions.v1.bedrock.info;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class BedrockInfoService {
-}
--- src/main/java/de/timmi6790/mpstats/api/versions/v1/bedrock/info/models/BedrockGamesModel.java
+++
@@ -1,9 +0,0 @@
-package de.timmi6790.mpstats.api.versions.v1.bedrock.info.models;
-
-import lombok.Data;
-
-@Data
-public class BedrockGamesModel {
-    private final String name;
-    private final boolean removedGame;
-}

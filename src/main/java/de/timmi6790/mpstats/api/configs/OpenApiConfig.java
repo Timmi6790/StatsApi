8ce4878e7c89c773e3329b47098af6ceb914a2a3
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-04-19 17:09:47 +0200

    Refactor OpenApiConfig
    - Bump version to 4.0.0
    - Remove hardcoded tags

--- src/main/java/de/timmi6790/mpstats/api/configs/OpenApiConfig.java
+++ src/main/java/de/timmi6790/mpstats/api/configs/OpenApiConfig.java
@@ -5,24 +5,19 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+
 
 @Configuration
 public class OpenApiConfig {
-    public static final String TAG_WEBSITE = "Website";
-
-    public static final String TAG_JAVA = "Java";
-    public static final String TAG_JAVA_INTERNAL = "Java - Internal";
-
-    public static final String TAG_BEDROCK = "Bedrock";
-    public static final String TAG_BEDROCK_INTERNAL = "Bedrock - Internal";
-
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
-                .info(new Info()
-                        .version("3.1.0")
-                        .title("StatsBot Stats API")
-                        .description("The backend api behind the StatsBot")
-                );
+                .info(
+                        new Info()
+                                .version("4.0.0")
+                                .title("StatsBot Stats API")
+                                .description("The backend api behind the StatsBot")
+                ).servers(new ArrayList<>());
     }
 }

b1cbeac530d28bf859f873abb1f4c75e77df791f
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-04-21 08:45:40 +0200

    Add basic FilterController

---
+++ src/main/java/de/timmi6790/mpstats/api/versions/v1/common/filter/FilterController.java
@@ -0,0 +1,71 @@
+package de.timmi6790.mpstats.api.versions.v1.common.filter;
+
+import de.timmi6790.mpstats.api.versions.v1.common.filter.repository.models.Filter;
+import de.timmi6790.mpstats.api.versions.v1.common.leaderboard.LeaderboardService;
+import de.timmi6790.mpstats.api.versions.v1.common.leaderboard.repository.models.Leaderboard;
+import de.timmi6790.mpstats.api.versions.v1.common.player.PlayerService;
+import de.timmi6790.mpstats.api.versions.v1.common.player.models.Player;
+import de.timmi6790.mpstats.api.versions.v1.common.player.models.RepositoryPlayer;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Getter(value = AccessLevel.PROTECTED)
+public class FilterController<P extends Player & RepositoryPlayer, S extends PlayerService<P>> {
+    private final FilterService<P, S> filterService;
+
+    protected LeaderboardService getLeaderboardService() {
+        return this.filterService.getLeaderboardService();
+    }
+
+    protected S getPlayerService() {
+        return this.filterService.getPlayerService();
+    }
+
+    @GetMapping
+    @Operation(summary = "Find all available filters")
+    public List<Filter<P>> getFilters() {
+        return this.filterService.getFilters();
+    }
+
+    @PostMapping(value = "/{gameName}/{statName}/{boardName}/{playerName}")
+    @Operation(summary = "Create a new filter")
+    public Optional<Filter<P>> createFilter(@PathVariable final String gameName,
+                                            @PathVariable final String statName,
+                                            @PathVariable final String boardName,
+                                            @PathVariable final String playerName,
+                                            @RequestParam final String reason,
+                                            @RequestParam final LocalDateTime filterStart,
+                                            @RequestParam final LocalDateTime filterEnd) {
+        // TODO: Add spring security
+
+        final Optional<Leaderboard> leaderboardOpt = this.getLeaderboardService().getLeaderboard(gameName, statName, boardName);
+        if (!leaderboardOpt.isPresent()) {
+            return Optional.empty();
+        }
+
+        final Optional<P> playerOpt = this.getPlayerService().getPlayer(playerName);
+        if (!playerOpt.isPresent()) {
+            return Optional.empty();
+        }
+
+        final Filter<P> filter = this.filterService.addFilter(playerOpt.get(), leaderboardOpt.get(), reason, filterStart, filterEnd);
+        return Optional.of(filter);
+    }
+
+    @DeleteMapping(value = "/{gameName}/{statName}/{boardName}/{playerName}")
+    @Operation(summary = "Remove a filter")
+    public void removeFilter(@PathVariable final String gameName,
+                             @PathVariable final String statName,
+                             @PathVariable final String boardName,
+                             @PathVariable final String playerName) {
+        // TODO: Add
+    }
+}

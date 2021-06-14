da8b42b8f18d83633d794be0f4925237f993909f
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-06-14 10:12:45 +0200

    Add HikariCP

--- src/main/java/de/timmi6790/mpstats/api/configs/DatabaseConfiguration.java
+++ src/main/java/de/timmi6790/mpstats/api/configs/DatabaseConfiguration.java
@@ -1,6 +1,9 @@
 package de.timmi6790.mpstats.api.configs;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import de.timmi6790.mpstats.api.Config;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.flywaydb.core.Flyway;
 import org.jdbi.v3.core.Jdbi;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +22,7 @@ public class DatabaseConfiguration {
     }
 
     @Bean
-    public Jdbi jdbi(final Config config) {
+    public Jdbi jdbi(final Config config, final MeterRegistry registry) {
         final Config.RepositoryConfig databaseConfig = config.getRepository();
 
         setupDatabase(
@@ -27,10 +30,22 @@ public class DatabaseConfiguration {
                 databaseConfig.getName(),
                 databaseConfig.getPassword()
         );
-        return Jdbi.create(
-                databaseConfig.getUrl(),
-                databaseConfig.getName(),
-                databaseConfig.getPassword()
-        );
+
+        final HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(databaseConfig.getUrl());
+        hikariConfig.setUsername(databaseConfig.getName());
+        hikariConfig.setPassword(databaseConfig.getPassword());
+        
+        hikariConfig.addDataSourceProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
+        // I'm not sure if those properties even work for postgres
+        hikariConfig.addDataSourceProperty("cachePrepStmts", true);
+        hikariConfig.addDataSourceProperty("prepStmtCacheSize", 250);
+        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
+        hikariConfig.addDataSourceProperty("useServerPrepStmts", true);
+
+        // Register metrics
+        hikariConfig.setMetricRegistry(registry);
+
+        return Jdbi.create(new HikariDataSource(hikariConfig));
     }
 }
